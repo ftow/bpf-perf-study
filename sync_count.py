8cb67c91c1c3a85ff39e2b866782ff7e9d1020b8
DW <49493541+ftow@users.noreply.github.com>
2020-03-13 01:59:59 -0700

    Add files via upload

---
+++ sync_count.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+#
+# sync_count.py    Trace how many sync syscalls.
+#                   For Linux, uses BCC, eBPF. Embedded C.
+#
+# Written as a basic excercise.
+#
+# Copyright 2016 Netflix, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License")
+
+from __future__ import print_function
+from bcc import BPF
+from bcc.utils import printb
+from time import sleep
+
+# load BPF program
+b = BPF(text="""
+#include <uapi/linux/ptrace.h>
+
+BPF_HASH(table);
+
+int do_trace(struct pt_regs *ctx) {
+    u64 key = 0;
+    table.increment(key);
+    return 0;
+}
+""")
+
+b.attach_kprobe(event=b.get_syscall_fnname("sync"), fn_name="do_trace")
+print("Counting syncs... Ctrl-C to end")
+
+try:
+    sleep(99999999)
+except KeyboardInterrupt:
+    for k, v in b["table"].items():
+        print("\nThere are %u sync calls\n" % (v.value))
+        # print("the key is %s" % (str(k)))
+
+
+
